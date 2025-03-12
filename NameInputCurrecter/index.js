40e8c3b484e459630f94e5e53081dfdf4f477587
abrarshakilahmed <shakilppki123@gmail.com>
2025-03-12 20:48:52 +0600

    done

---
+++ NameInputCurrecter/index.js
@@ -0,0 +1,15 @@
+const UserInputName = "e2xample 34hasan   #";
+function NameCurrector (name){
+    let correctName = name.trim()
+    correctName = correctName.split(" ")
+    correctName[0] = correctName[0].charAt(0).toUpperCase() + correctName[0].slice(1).toLowerCase()
+    correctName[1] = correctName[1].charAt(0).toUpperCase() + correctName[1].slice(1).toLowerCase()
+    correctName = correctName[0] + " " + correctName[1]
+    console.log(correctName)
+}
+function removeSpecialCharacters(name) {
+    return name.replace(/[^a-zA-Z\s]/g, '');
+}
+
+const cleanedName = removeSpecialCharacters(UserInputName);
+NameCurrector(cleanedName)
